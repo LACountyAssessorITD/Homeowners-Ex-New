9a0a4baa68b4069b6c2b5dbc4aec443cb17a18f8
Daniel <dspencer@assessor.lacounty.gov>
2019-06-03 10:27:16 -0700

    push

---
+++ HomeOwners-Exemption/Views/Services/IUserService.cs
@@ -0,0 +1,23 @@
+﻿
+using HomeOwners_Exemption.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeOwners_Exemption.Services
+{
+    public interface IUserService
+    {
+        Task<bool> ValidateCredentials(string username, string password, homeownerContext context, out User user);
+    }
+    public class User
+    {
+        public User(string username)
+        {
+
+            Username = username;
+        }
+        public string Username { get; }
+    }
+}
