afb5b96bd9a67bbaa16a865ac4017fcbc4f83339
willielam <44653452+willielam@users.noreply.github.com>
2019-06-03 07:04:12 -0700

    wlam

---
+++ HomeOwners-Exemption/Models/ProcessClaim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeOwners_Exemption.Models
+{
+    public class ProcessClaim
+    {
+        public int ClaimID { get; set; }
+        public string ClaimStatus { get; set; }
+        public string Assignee { get; set; }
+
+        public string ClaimDate { get; set; }
+    }
+}
