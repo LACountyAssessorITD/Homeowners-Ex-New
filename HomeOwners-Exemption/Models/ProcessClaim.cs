3030149047fe8a6d438d205b895074ff2823a0dd
willielam <44653452+willielam@users.noreply.github.com>
2019-06-09 22:50:10 -0700

    wlam

--- HomeOwners-Exemption/Models/ProcessClaim.cs
+++ HomeOwners-Exemption/Models/ProcessClaim.cs
@@ -12,23 +12,10 @@ namespace HomeOwners_Exemption.Models
     public class ProcessClaim
     {
         public string ClaimStatus { get; set; }
-        //public int ClaimID { get; set; }
-        //public int AIN { get; set; }
         public string ClaimReceivedDate { get; set; }
         public string Supervisor { get; set; }
         public string Staff { get; set; }
-        //public IEnumerable<ClaimInfoClass> ClaimInfo { get; set; }
         public IEnumerable<SelectListItem> Supervisors { get; set; }
         public IEnumerable<SelectListItem> Staffs { get; set; }
-
-        //public class ClaimInfoClass
-        //{
-        //    public string ClaimStatus1 { get; set; }
-        //    public int ClaimID1 { get; set; }
-        //    public int AIN1 { get; set; }
-        //    public string Assignor1 { get; set; }
-        //    public string Assignee1 { get; set; }
-        //    public string ClaimReceivedDate1 { get; set; }
-        //}
     }
 }
